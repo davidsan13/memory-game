f118fa7a48deb54aa9b8c665285f066af7f1e192
davidsan13 <sandavid08@yahoo.com>
2023-05-05 22:28:45 -0700

    setup project prettier/eslint airbnb style

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import uniqid from 'uniqid';
+
+function Card() {
+  const [card, setCard] = useState(
+    {
+      clicked: false,
+      img: '',
+      name: '',
+      id: uniqid()
+    }
+  )
+  return (
+
+  );
+}
