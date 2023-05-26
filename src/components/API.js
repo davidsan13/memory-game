d405d8f567d08e3471c6e14827528e273bf4e1a6
davidsan13 <sandavid08@yahoo.com>
2023-05-26 11:21:25 -0700

    api

--- src/components/API.js
+++ src/components/API.js
@@ -17,7 +17,7 @@ export default function Pokemon() {
     } else {
       console.log('Fetching Data');
       const promises = [];
-      for (let i = 1; i <= 2; i += 1) {
+      for (let i = 0; i <= 20; i += 1) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
         promises.push(fetch(url));
       }
