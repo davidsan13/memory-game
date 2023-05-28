8eb35230acff0cc6975c21bb8f4da8e008042ae4
davidsan13 <sandavid08@yahoo.com>
2023-05-27 20:34:52 -0700

    refactor api component

--- src/components/API.js
+++ src/components/API.js
@@ -4,9 +4,12 @@ import uniqid from 'uniqid';
 import Card from './Card';
 
 export default function Pokemon() {
-  const [allPokemon, setPokemon] = useState([])
+  const initialData = JSON.parse(localStorage.getItem('pokemonList'));
+  const [allPokemon, setPokemon] = useState(initialData || []);
   useEffect(() => {
-    getPokemons();
+    // const pokemonList = getPokemons();
+    // updatePokemon(pokemonList);
+    localSave();
   }, []);
 
   async function getPokemons() {
@@ -14,28 +17,73 @@ export default function Pokemon() {
     if (pokemonList) {
       pokemonList = JSON.parse(pokemonList);
       console.log('Has Data');
+      console.log(pokemonList);
     } else {
       console.log('Fetching Data');
       const promises = [];
-      for (let i = 0; i <= 20; i += 1) {
+      for (let i = 0; i <= 1; i += 1) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
         promises.push(fetch(url));
       }
       const response = await Promise.all(promises);
-      pokemonList = [];
-      response.forEach(async (item) => {
+      // pokemonList = [];
+    //  response.forEach(async (item) => {
+    //     const pokemonData = await item.json();
+    //     pokemonList.push(pokemonData);
+    //     localStorage.setItem('pokemonList', JSON.stringify(pokemonList));
+    //   });
+ 
+      pokemonList = response.map(async (item) => {
         const pokemonData = await item.json();
         pokemonList.push(pokemonData);
-        localStorage.setItem('pokemonList', JSON.stringify(pokemonList));
       });
+      setTimeout(() => {localStorage.setItem('pokemonList', JSON.stringify(pokemonList))}, 10000);
     }
-    updatePokemon(pokemonList);
-    console.log(1)
+    
+
+    console.log(pokemonList);
+    return pokemonList;
   }
 
-  function updatePokemon(pokemonArr) {
-    console.log(pokemonArr)
-    pokemonArr.forEach((item) => setPokemon((prevState) => [...prevState, {
+  async function getData() {
+    const pokemons = [];
+    const results = [];
+    for (let i = 1; i <= 2; i += 1) {
+      const url = `https://pokeapi.co/api/v2/pokemon/${i}/`;
+      results.push(fetch(url));
+    }
+    const response = await Promise.all(results);
+    response.forEach(async (item) => {
+      const data = await item.json();
+      const pokemon = {
+        click: false,
+        img: data.sprites.front_default,
+        name: data.name,
+      };
+      console.log(pokemon)
+      pokemons.push(pokemon);
+      localStorage.setItem('pokemonList', JSON.stringify(pokemons));
+      setPokemon((prevState) => [...prevState, pokemon]);
+    });
+    console.log(pokemons);
+   
+    return pokemons;
+  }
+
+  async function localSave() {
+    let pokemonList = localStorage.getItem('pokemonList');
+    if (pokemonList == null) {
+      await getData();
+      // pokemonList = JSON.parse(pokemonList);
+      // console.log('Has Data');
+    }
+  }
+  async function updatePokemon() {
+    let pokemonList = localStorage.getItem('pokemonList');
+    console.log(pokemonList)
+    pokemonList = JSON.parse(pokemonList);
+    pokemonList.forEach((item) => setPokemon((prevState) => [...prevState, {
+      click: false,
       img: item.sprites.front_default,
       name: item.name,
       id: uniqid(),
