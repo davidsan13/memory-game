92ef9896387dc22dfd8ce2debe8a1c28c3e0e2a9
David <sandavid08@yahoo.com>
2023-08-15 15:39:05 -0700

    build

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import uniqid from 'uniqid';
-// import './App.css';
 import Main from './components/Main';
 import Header from './components/Header';
 import Loading from './components/Loading';
